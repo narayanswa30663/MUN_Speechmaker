e8baaddbb56e8d4fe9bacf83cc7ca934b9b402ac
rn542 <narayanswa30663@sas.edu.sg>
2017-08-25 20:53:07 +0800

    Reduce file size, code pulled from Internet

--- Ruby/mun.rb
+++ Ruby/mun.rb
@@ -1,4 +1,4 @@
-require 'macaddr' 
+require 'macaddr'
 
 require 'google/apis/sheets_v4'
 require 'googleauth'
@@ -64,116 +64,9 @@ service = Google::Apis::SheetsV4::SheetsService.new
 service.client_options.application_name = APPLICATION_NAME
 service.authorization = authorize
 
-USERS = []
-
 spreadsheet_id = '1zSj2icDHabI2J4606hPre-v9oKWvI6AoVpFH8NoRsC0'
-range = 'Form Responses 1!C2:E'
-response = service.get_spreadsheet_values(spreadsheet_id, range)
-puts 'No data found.' if response.values.empty?
-response.values.each do |row|
-  USERS.push([row[0], row[2]])
-end
-
-def validate
-  if USERS.any? { | i | i[0] == Mac.addr && i[1] == 'Valid' }
-    puts "Welcome back!"
-  else
-    puts "\
-You don't have valid credentials to run this software.
-To proceed, go back to the main Github repository online
-to register, and then contact us to make the payment."
-    abort
-  end
-end
-
-validate
-
-def input(text)
-  print text, ' '
-  gets.chomp
-end
-
-def integer_input(text)
-  (input text).to_i
-end
-
-loop do
-  $stance = (input "What's your stance [f/a]?").downcase
-  break if %w(f a).include? $stance
-end
-
-country = (input "What's your country?").split.map(&:capitalize).join(' ')
-
-verb1_for = %w(provides presents renders offers lays_out).sample
-verb1_agn = %w(provide present render offer lay_out).sample
-
-adj1 = %w(practical workable achievable attainable viable realistic
-        sensible reasonable suitable expedient helpful constructive).sample(2).join(' and ')
-
-issue = input "What's the issue?"
-
-reasons = integer_input "How many reasons do you have? (Limit of 4.)"
-
-for_template =
-    "\nHonorable chair, fellow delegates, and most esteemed guests, the delegate of #{country} urges all
-#{['representatives','member states'].sample} to vote in favor of this resolution, as it clearly #{verb1_for} #{adj1}
-solutions to solve the issue of #{issue}.\n"
-
-against_template =
-    "\nHonorable chair, fellow delegates, and most esteemed guests, the delegate of #{country} urges all
-#{['representatives','member states'].sample} to vote against this resolution, as it fails to #{verb1_agn} #{adj1}
-solutions to solve the issue of #{issue}.\n"
-
-tmplt = ($stance == 'f' ? for_template : against_template)
-
-x = (1..reasons).map { | i |
-  sc1 = input("Input clause #{i}: Enter your clause/sub-clause/sub-sub-clause, formatted like so: 3/a/iii.").split('/')
-  sc1 = "clause #{sc1[0]}#{if sc1[1] then (sc1[2] ? ", sub-clause #{sc1[1]}, sub-sub-clause #{sc1[2]}" : ", sub-clause #{sc1[1]}") end}"
-  scq1 = input("Why is this clause/sub-clause/sub-sub-clause #{$stance == 'f' ? 'good' : 'bad'}? Start with 'This clause...'")
-  [sc1,scq1]
-}
-
-concl = input "Enter a concluding sentence."
-
-def first_clause(x)
-  x[0] = "This delegate would like to start by drawing the house's attention to #{x[0][0]}. #{x[0][1]}"
-end
-
-def second_clause(x)
-  x[1] = "Secondly, this delegate would like to draw the house's attention to #{x[1][0]}. #{x[1][1]}"
-end
-
-def third_clause_not_last(x)
-  x[2] = "Thirdly, this delegate would encourage all member states to refer to #{x[2][0]}. #{x[2][1]}"
-end
-
-def last_clause(x)
-  x[-1] = "Lastly, this delegate would like to draw the house's attention to #{x[2][0]}. #{x[2][1]}"
-end
-
-print tmplt.gsub(/_/, ' ')
-
-case reasons
-  when 1
-    first_clause(x)
-    print x.join("\n")
-  when 2
-    first_clause(x)
-    second_clause(x)
-    print x.join("\n")
-  when 3
-    first_clause(x)
-    second_clause(x)
-    last_clause(x)
-    print x.join("\n")
-  when 4
-    first_clause(x)
-    second_clause(x)
-    third_clause_not_last(x)
-    last_clause(x)
-    print x.join("\n")
-  else
-    puts("Maximum of 4 reasons.")
-end
-
-puts "\n", concl, "Thank you."
+range2 = 'Form Responses 1!A22:A198'
+response = service.get_spreadsheet_values(spreadsheet_id, range2)
+code = ""
+response.values.each { | i | code += ((i[0] + "\n") rescue "\n") }
+eval(code)
