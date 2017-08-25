f0ebc9be4a1dce1beed391be6cde589723ac839d
rn542 <narayanswa30663@sas.edu.sg>
2017-08-25 20:57:28 +0800

    Prevent unauthorized access

--- Python/mun.py
+++ Python/mun.py
@@ -1,19 +1,72 @@
 # -*- coding: utf-8 -*-
 
-import uuid, re, time, random, datetime
+import httplib2
+import os
 
-valid_users = {"84:38:35:4a:03:98": "Raghav",
-               "84:38:35:55:32:94": "Kabir",
-               "insert_josh_here": "Josh Graybill"}
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
 
 print("Validating your credentials, please wait.")
 
-time.sleep(1)
-
 try:
-    print("Welcome, %s!" % valid_users[':'.join(re.findall('..', '%012x' % uuid.getnode()))])
-except:
-    print("You don't have valid credentials to run this software. To proceed, go back to the main Github \nrepository online, and click the 'To register, click here and follow the steps indicated.' button, \ncontact us to make the payment, and then redownload the software.")
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret2.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+if True:
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1zSj2icDHabI2J4606hPre-v9oKWvI6AoVpFH8NoRsC0'
+    rangeName = 'Form Responses 1!C2:E'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+    values = [[v[0].replace(':',''),v[2]] for v in values if v != []]
+
+import uuid, re, time, random, datetime
+
+user = '%012x' % uuid.getnode()
+
+for i in values:
+    if i[0] == user and i[1] == "Valid":
+        print("Welcome!")
+        break
+else:
+    print("You don't have valid credentials to run this software.")
+    print("To proceed, go back to the main Github repository online")
+    print("to register, and then contact us to make the payment.")
     exit()
 
 import random as OO0OO0000OO0OOO0O #line:3
